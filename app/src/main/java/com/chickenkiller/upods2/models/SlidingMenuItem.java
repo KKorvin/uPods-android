a858b3a4032c11ea45ca23b3a85a03d20a8eafb2
Alex Berber <berber.it@gmail.com>
2015-07-04 20:07:59 +0300

    - Icon menu added - Devider position selected

--- app/src/main/java/com/chickenkiller/upods2/models/SlidingMenuItem.java
+++ app/src/main/java/com/chickenkiller/upods2/models/SlidingMenuItem.java
@@ -48,10 +48,10 @@ public class SlidingMenuItem {
         allItems.add(new SlidingMenuItem(mContext.getString(R.string.profile_my_profile), R.drawable.ic_account_circle_black_36dp));
         allItems.add(new SlidingMenuItem(mContext.getString(R.string.radio_main), R.drawable.ic_radio_black_36dp));
         allItems.add(new SlidingMenuItem(mContext.getString(R.string.podcasts_main), R.drawable.ic_speaker_black_36dp));
-        allItems.add(new SlidingMenuItem(mContext.getString(R.string.cloud_sync), R.drawable.ic_cloud_upload_black_36dp));
-        allItems.add(new SlidingMenuItem(mContext.getString(R.string.main_rate_app), R.drawable.ic_favorite_black_36dp, true));
+        allItems.add(new SlidingMenuItem(mContext.getString(R.string.cloud_sync), R.drawable.ic_cloud_upload_black_36dp, true));
+        allItems.add(new SlidingMenuItem(mContext.getString(R.string.main_rate_app), R.drawable.ic_favorite_black_36dp));
         allItems.add(new SlidingMenuItem(mContext.getString(R.string.main_about), R.drawable.ic_android_black_36dp));
-        allItems.add(new SlidingMenuItem(mContext.getString(R.string.main_help), R.drawable.ic_help_black_36dp));
+        allItems.add(new SlidingMenuItem(mContext.getString(R.string.main_help), R.drawable.ic_help_black_36dp, true));
         allItems.add(new SlidingMenuItem(mContext.getString(R.string.main_settings), R.drawable.ic_settings_black_36dp));
         return allItems;
     }
