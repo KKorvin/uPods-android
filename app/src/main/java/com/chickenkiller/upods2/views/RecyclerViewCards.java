23c49d1b14d09a50c1389b67d131d377dde3623d
alonzilberman <alon.milo@gmail.com>
2015-07-05 23:16:55 +0300

    RecircleView cards

---
+++ app/src/main/java/com/chickenkiller/upods2/views/RecyclerViewCards.java
@@ -0,0 +1,53 @@
+package com.chickenkiller.upods2.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by alonzilberman on 7/5/15.
+ */
+public class RecyclerViewCards extends RecyclerView {
+    private GridLayoutManager manager;
+    private int columnWidth = -1;
+
+    public RecyclerViewCards(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public RecyclerViewCards(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public RecyclerViewCards(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            int[] attrsArray = {
+                    android.R.attr.columnWidth
+            };
+            TypedArray array = context.obtainStyledAttributes(attrs, attrsArray);
+            columnWidth = array.getDimensionPixelSize(0, -1);
+            array.recycle();
+        }
+
+        manager = new GridLayoutManager(getContext(), 1);
+        setLayoutManager(manager);
+    }
+
+    @Override
+    protected void onMeasure(int widthSpec, int heightSpec) {
+        super.onMeasure(widthSpec, heightSpec);
+        if (columnWidth > 0) {
+            int spanCount = Math.max(1, getMeasuredWidth() / columnWidth);
+            manager.setSpanCount(spanCount);
+        }
+    }
+}
