96a087b5f26429acb6dc338368f322857ec03642
alonzilberman <alon.milo@gmail.com>
2015-07-06 00:37:07 +0300

    basic banner view fiew fixes

--- app/src/main/java/com/chickenkiller/upods2/controllers/BanerItemsAdapter.java
+++ app/src/main/java/com/chickenkiller/upods2/controllers/BanerItemsAdapter.java
@@ -5,11 +5,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.chickenkiller.upods2.R;
 import com.chickenkiller.upods2.models.BanerItem;
+import com.chickenkiller.upods2.views.ImageViewBanner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,11 +24,11 @@ public class BanerItemsAdapter extends RecyclerView.Adapter<BanerItemsAdapter.Vi
     private Context mContext;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView imgBaner;
+        public ImageViewBanner imgBaner;
 
         public ViewHolder(View view) {
             super(view);
-            this.imgBaner = (ImageView) view.findViewById(R.id.imgBaner);
+            this.imgBaner = (ImageViewBanner) view.findViewById(R.id.imgBaner);
         }
     }
 
@@ -51,7 +51,7 @@ public class BanerItemsAdapter extends RecyclerView.Adapter<BanerItemsAdapter.Vi
     @Override
     public void onBindViewHolder(BanerItemsAdapter.ViewHolder holder, int position) {
         BanerItem currentItem = items.get(position);
-        Glide.with(mContext).load(currentItem.getImageUrl()).centerCrop().crossFade().into(holder.imgBaner);
+        Glide.with(mContext).load(currentItem.getImageUrl()).crossFade().into(holder.imgBaner);
         holder.itemView.setTag(currentItem);
     }
 
