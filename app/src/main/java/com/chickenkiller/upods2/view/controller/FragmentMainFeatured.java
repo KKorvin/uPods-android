533c621847e9ed058bc0e514354c4a3ebf1feefe
alonzilberman <alon.milo@gmail.com>
2015-07-13 22:44:23 +0300

    GridViewSpacing added

--- app/src/main/java/com/chickenkiller/upods2/view/controller/FragmentMainFeatured.java
+++ app/src/main/java/com/chickenkiller/upods2/view/controller/FragmentMainFeatured.java
@@ -1,16 +1,14 @@
 package com.chickenkiller.upods2.view.controller;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.chickenkiller.upods2.R;
+import com.chickenkiller.upods2.controllers.GridSpacingItemDecoration;
 import com.chickenkiller.upods2.controllers.MediaItemsAdapter;
 import com.chickenkiller.upods2.controllers.RadioTopManager;
 import com.chickenkiller.upods2.interfaces.INetworkUIupdater;
@@ -31,47 +29,15 @@ public class FragmentMainFeatured extends Fragment {
     private AutofitRecyclerView rvMain;
     private MediaItemsAdapter mediaItemsAdapter;
 
-    public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
-        private int space;
-
-        public SpacesItemDecoration(int space) {
-            this.space = space;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view,
-                                   RecyclerView parent, RecyclerView.State state) {
-            int spanCount = rvMain.getSpanCount();
-            int position = rvMain.getChildAdapterPosition(view);
-            if (mediaItemsAdapter.getItemViewType(position) != MediaItemsAdapter.ITEM) {
-                outRect.bottom = 0;
-                outRect.top = 0;
-                outRect.left = 0;
-                outRect.right = 0;
-            } else {
-                int spanIndex = (position - mediaItemsAdapter.getOneColumnItemsCount()) % spanCount;
-                outRect.bottom = space;
-                outRect.top = space;
-                if (spanIndex == 1) {
-                    outRect.left = space / 2;
-                    outRect.right = space / 2;
-                } else {
-                    outRect.left = space;
-                    outRect.right = space;
-                }
-            }
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_main_featured, container, false);
-        Activity mActivity = getActivity();
 
         rvMain = (AutofitRecyclerView) view.findViewById(R.id.rvMain);
         rvMain.setHasFixedSize(true);
-        rvMain.addItemDecoration(new SpacesItemDecoration(20));
+        rvMain.addItemDecoration(new GridSpacingItemDecoration(3, 25, true));
+
         showTops();
 
         return view;
