7351467ea92c11783222378b2b0965518e6e0b57
alonzilberman <alon.milo@gmail.com>
2015-07-21 22:28:16 +0300

    Fragment is moovable now

--- app/src/main/java/com/chickenkiller/upods2/view/controller/FragmentRadioItemDetails.java
+++ app/src/main/java/com/chickenkiller/upods2/view/controller/FragmentRadioItemDetails.java
@@ -3,10 +3,12 @@ package com.chickenkiller.upods2.view.controller;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -16,27 +18,29 @@ import com.chickenkiller.upods2.models.RadioItem;
 /**
  * Created by alonzilberman on 7/8/15.
  */
-public class FragmentRadioItemDetails extends Fragment {
+public class FragmentRadioItemDetails extends Fragment implements View.OnTouchListener {
+    private static final int MAGIC_NUMBER = -250; //Don't know what it does
     public static String TAG = "media_details";
 
     private RadioItem radioItem;
 
-    private LinearLayout lnDetailedTransparentHeader;
+    private RelativeLayout rlDetailedContent;
     private TextView tvDetailedDescription;
     private TextView tvDetailedHeader;
     private ImageView imgDetailedHeader;
     private ImageView imgDetailedTopCover;
+    private int moveDeltayY;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_media_details, container, false);
-        lnDetailedTransparentHeader = (LinearLayout) view.findViewById(R.id.lnDetailedTransparentHeader);
+        rlDetailedContent = (RelativeLayout) view.findViewById(R.id.rlDetailedContent);
         tvDetailedDescription = (TextView) view.findViewById(R.id.tvDetailedDescription);
         tvDetailedHeader = (TextView) view.findViewById(R.id.tvDetailedHeader);
         imgDetailedHeader = (ImageView) view.findViewById(R.id.imgDetailedHeader);
         imgDetailedTopCover = (ImageView) view.findViewById(R.id.imgDetailedCover);
-
+        moveDeltayY = 0;
 
         if (radioItem != null) {
             Glide.with(getActivity()).load("http://www.linuxspace.org/wp-content/uploads/2015/examples/example_4.png").centerCrop().crossFade().into(imgDetailedHeader);
@@ -44,11 +48,37 @@ public class FragmentRadioItemDetails extends Fragment {
             tvDetailedHeader.setText(radioItem.getName());
             tvDetailedDescription.setText(radioItem.getDescription());
         }
-
+        rlDetailedContent.setOnTouchListener(this);
         return view;
     }
 
     public void setRadioItem(RadioItem radioItem) {
         this.radioItem = radioItem;
     }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        final int Y = (int) event.getRawY();
+        LinearLayout.LayoutParams lParams = (LinearLayout.LayoutParams) rlDetailedContent.getLayoutParams();
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                moveDeltayY = Y - lParams.topMargin;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                lParams.topMargin = Y - moveDeltayY;
+                lParams.bottomMargin = MAGIC_NUMBER;
+                view.setLayoutParams(lParams);
+                break;
+        }
+        return true;
+    }
+
+
 }
