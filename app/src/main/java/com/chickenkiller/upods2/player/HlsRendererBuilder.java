552d43bc6ac987875924cdb109968c98bdb95267
alonzilberman <alon.milo@gmail.com>
2015-07-29 00:33:07 +0300

    Exo player demo

---
+++ app/src/main/java/com/chickenkiller/upods2/player/HlsRendererBuilder.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.chickenkiller.upods2.player;
+
+import android.content.Context;
+import android.media.MediaCodec;
+import android.os.Handler;
+
+import com.google.android.exoplayer.DefaultLoadControl;
+import com.google.android.exoplayer.LoadControl;
+import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.MediaCodecUtil.DecoderQueryException;
+import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
+import com.google.android.exoplayer.TrackRenderer;
+import com.google.android.exoplayer.audio.AudioCapabilities;
+import com.google.android.exoplayer.chunk.VideoFormatSelectorUtil;
+import com.google.android.exoplayer.hls.HlsChunkSource;
+import com.google.android.exoplayer.hls.HlsMasterPlaylist;
+import com.google.android.exoplayer.hls.HlsPlaylist;
+import com.google.android.exoplayer.hls.HlsPlaylistParser;
+import com.google.android.exoplayer.hls.HlsSampleSource;
+import com.google.android.exoplayer.metadata.Id3Parser;
+import com.google.android.exoplayer.metadata.MetadataTrackRenderer;
+import com.google.android.exoplayer.text.eia608.Eia608TrackRenderer;
+import com.google.android.exoplayer.upstream.DataSource;
+import com.google.android.exoplayer.upstream.DefaultAllocator;
+import com.google.android.exoplayer.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer.upstream.DefaultUriDataSource;
+import com.google.android.exoplayer.util.ManifestFetcher;
+import com.google.android.exoplayer.util.ManifestFetcher.ManifestCallback;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * A {@link MediaPlayer.RendererBuilder} for HLS.
+ */
+public class HlsRendererBuilder implements MediaPlayer.RendererBuilder, ManifestCallback<HlsPlaylist> {
+
+  private static final int BUFFER_SEGMENT_SIZE = 256 * 1024;
+  private static final int BUFFER_SEGMENTS = 64;
+
+  private final Context context;
+  private final String userAgent;
+  private final String url;
+  private final AudioCapabilities audioCapabilities;
+
+  private MediaPlayer player;
+  private MediaPlayer.RendererBuilderCallback callback;
+
+  public HlsRendererBuilder(Context context, String userAgent, String url,
+      AudioCapabilities audioCapabilities) {
+    this.context = context;
+    this.userAgent = userAgent;
+    this.url = url;
+    this.audioCapabilities = audioCapabilities;
+  }
+
+  @Override
+  public void buildRenderers(MediaPlayer player, MediaPlayer.RendererBuilderCallback callback) {
+    this.player = player;
+    this.callback = callback;
+    HlsPlaylistParser parser = new HlsPlaylistParser();
+    ManifestFetcher<HlsPlaylist> playlistFetcher = new ManifestFetcher<>(url,
+        new DefaultUriDataSource(context, userAgent), parser);
+    playlistFetcher.singleLoad(player.getMainHandler().getLooper(), this);
+  }
+
+  @Override
+  public void onSingleManifestError(IOException e) {
+    callback.onRenderersError(e);
+  }
+
+  @Override
+  public void onSingleManifest(HlsPlaylist manifest) {
+    Handler mainHandler = player.getMainHandler();
+    LoadControl loadControl = new DefaultLoadControl(new DefaultAllocator(BUFFER_SEGMENT_SIZE));
+    DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+
+    int[] variantIndices = null;
+    if (manifest instanceof HlsMasterPlaylist) {
+      HlsMasterPlaylist masterPlaylist = (HlsMasterPlaylist) manifest;
+      try {
+        variantIndices = VideoFormatSelectorUtil.selectVideoFormatsForDefaultDisplay(
+            context, masterPlaylist.variants, null, false);
+      } catch (DecoderQueryException e) {
+        callback.onRenderersError(e);
+        return;
+      }
+    }
+
+    DataSource dataSource = new DefaultUriDataSource(context, bandwidthMeter, userAgent);
+    HlsChunkSource chunkSource = new HlsChunkSource(dataSource, url, manifest, bandwidthMeter,
+        variantIndices, HlsChunkSource.ADAPTIVE_MODE_SPLICE, audioCapabilities);
+    HlsSampleSource sampleSource = new HlsSampleSource(chunkSource, loadControl,
+        BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE, true, mainHandler, player, MediaPlayer.TYPE_VIDEO);
+    MediaCodecVideoTrackRenderer videoRenderer = new MediaCodecVideoTrackRenderer(sampleSource,
+        MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT, 5000, mainHandler, player, 50);
+    MediaCodecAudioTrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource);
+
+    MetadataTrackRenderer<Map<String, Object>> id3Renderer =
+        new MetadataTrackRenderer<>(sampleSource, new Id3Parser(), player, mainHandler.getLooper());
+
+    Eia608TrackRenderer closedCaptionRenderer = new Eia608TrackRenderer(sampleSource, player,
+        mainHandler.getLooper());
+
+    TrackRenderer[] renderers = new TrackRenderer[MediaPlayer.RENDERER_COUNT];
+    renderers[MediaPlayer.TYPE_VIDEO] = videoRenderer;
+    renderers[MediaPlayer.TYPE_AUDIO] = audioRenderer;
+    renderers[MediaPlayer.TYPE_METADATA] = id3Renderer;
+    renderers[MediaPlayer.TYPE_TEXT] = closedCaptionRenderer;
+    callback.onRenderers(null, null, renderers, bandwidthMeter);
+  }
+
+}
--- app/src/main/java/com/chickenkiller/upods2/view/controller/FragmentPlayer.java
+++ app/src/main/java/com/chickenkiller/upods2/view/controller/FragmentPlayer.java
@@ -2,6 +2,7 @@ package com.chickenkiller.upods2.view.controller;
 
 import android.app.Fragment;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,18 +18,39 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.chickenkiller.upods2.R;
 import com.chickenkiller.upods2.models.RadioItem;
+import com.chickenkiller.upods2.player.ExtractorRendererBuilder;
+import com.chickenkiller.upods2.player.MediaPlayer;
 import com.chickenkiller.upods2.utils.UIHelper;
+import com.google.android.exoplayer.extractor.mp3.Mp3Extractor;
+import com.google.android.exoplayer.extractor.mp4.FragmentedMp4Extractor;
+import com.google.android.exoplayer.extractor.mp4.Mp4Extractor;
+import com.google.android.exoplayer.extractor.ts.AdtsExtractor;
+import com.google.android.exoplayer.extractor.webm.WebmExtractor;
+import com.google.android.exoplayer.util.Util;
 
 /**
  * Created by alonzilberman on 7/27/15.
  */
 public class FragmentPlayer extends Fragment {
-
+    public static final int TYPE_DASH = 0;
+    public static final int TYPE_SS = 1;
+    public static final int TYPE_HLS = 2;
+    public static final int TYPE_MP4 = 3;
+    public static final int TYPE_MP3 = 4;
+    public static final int TYPE_FMP4 = 5;
+    public static final int TYPE_WEBM = 6;
+    public static final int TYPE_MKV = 7;
+    public static final int TYPE_TS = 8;
+    public static final int TYPE_AAC = 9;
+    public static final int TYPE_M4A = 10;
     public static String TAG = "fragmentPlayer";
+
     private Button btnPlay;
     private RadioItem radioItem;
     private ImageView imgPlayerCover;
     private RelativeLayout rlTopSectionBckg;
+    private int contentType;
+    private MediaPlayer player;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -53,9 +75,46 @@ public class FragmentPlayer extends Fragment {
                 rlTopSectionBckg.setBackgroundColor(dominantColor);
             }
         });
+        contentType = TYPE_MP3;
+        player = new MediaPlayer(getRendererBuilder());
+        player.prepare();
+        player.setPlayWhenReady(true);
     }
 
     public void setRadioItem(RadioItem radioItem) {
         this.radioItem = radioItem;
+    }
+
+    private MediaPlayer.RendererBuilder getRendererBuilder() {
+        Uri contentUri = Uri.parse(radioItem.getStreamUrl());
+        String userAgent = Util.getUserAgent(getActivity(), "ExoPlayerDemo");
+        switch (contentType) {
+            // case TYPE_SS:
+            //     return new SmoothStreamingRendererBuilder(getActivity(), userAgent, contentUri.toString(),
+            //             new SmoothStreamingTestMediaDrmCallback());
+            //case TYPE_DASH:
+            //    return new DashRendererBuilder(getActivity(), userAgent, contentUri.toString(),
+            //            new WidevineTestMediaDrmCallback(contentId), audioCapabilities);
+            //case TYPE_HLS:
+            //    return new HlsRendererBuilder(getActivity(), userAgent, contentUri.toString(), audioCapabilities);
+            // case TYPE_M4A: // There are no file format differences between M4A and MP4.
+            case TYPE_MP4:
+                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri, new Mp4Extractor());
+            case TYPE_MP3:
+                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri, new Mp3Extractor());
+            // case TYPE_TS:
+            //return new ExtractorRendererBuilder(getActivity(), userAgent, contentUri,
+            //         new TsExtractor(0, audioCapabilities));
+            case TYPE_AAC:
+                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri, new AdtsExtractor());
+            case TYPE_FMP4:
+                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri,
+                        new FragmentedMp4Extractor());
+            case TYPE_WEBM:
+            case TYPE_MKV:
+                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri, new WebmExtractor());
+            default:
+                throw new IllegalStateException("Unsupported type: " + contentType);
+        }
     }
 }
