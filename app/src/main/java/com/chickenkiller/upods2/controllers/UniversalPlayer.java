918a802eb109ca2d2becfb3ce0c179045966d1a6
alonzilberman <alon.milo@gmail.com>
2015-07-29 23:30:29 +0300

    Media player first step

---
+++ app/src/main/java/com/chickenkiller/upods2/controllers/UniversalPlayer.java
@@ -0,0 +1,82 @@
+package com.chickenkiller.upods2.controllers;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by alonzilberman on 7/29/15.
+ */
+public class UniversalPlayer implements MediaPlayer.OnPreparedListener {
+
+    public static UniversalPlayer universalPlayer;
+    private MediaPlayer mediaPlayer;
+    private MediaPlayer.OnPreparedListener preparedListener;
+
+    public boolean isPrepaired;
+
+
+    private UniversalPlayer() {
+    }
+
+    public static UniversalPlayer getInstance() {
+        if (universalPlayer == null) {
+            universalPlayer = new UniversalPlayer();
+        }
+        return universalPlayer;
+    }
+
+    public void setPreparedListener(MediaPlayer.OnPreparedListener preparedListener) {
+        this.preparedListener = preparedListener;
+    }
+
+    public void prepare(Context context, String url) {
+        isPrepaired = false;
+        try {
+            if (mediaPlayer == null) {
+                mediaPlayer = new MediaPlayer();
+            }
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.prepareAsync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        if (mediaPlayer != null && isPrepaired) {
+            mediaPlayer.start();
+        }
+    }
+
+    public void toggle() {
+        if (mediaPlayer != null && isPrepaired) {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.pause();
+            } else {
+                mediaPlayer.start();
+            }
+        }
+    }
+
+    public void pause() {
+        if (mediaPlayer != null && isPrepaired && mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        }
+    }
+
+    public void release() {
+        mediaPlayer.release();
+        preparedListener = null;
+        isPrepaired = false;
+        universalPlayer = null;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        this.isPrepaired = true;
+        if (this.preparedListener != null) {
+            this.preparedListener.onPrepared(mediaPlayer);
+        }
+    }
+}
--- app/src/main/java/com/chickenkiller/upods2/view/controller/FragmentPlayer.java
+++ app/src/main/java/com/chickenkiller/upods2/view/controller/FragmentPlayer.java
@@ -2,7 +2,8 @@ package com.chickenkiller.upods2.view.controller;
 
 import android.app.Fragment;
 import android.graphics.Bitmap;
-import android.net.Uri;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,51 +18,48 @@ import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.chickenkiller.upods2.R;
+import com.chickenkiller.upods2.controllers.UniversalPlayer;
 import com.chickenkiller.upods2.models.RadioItem;
-import com.chickenkiller.upods2.player.ExtractorRendererBuilder;
-import com.chickenkiller.upods2.player.MediaPlayer;
 import com.chickenkiller.upods2.utils.UIHelper;
-import com.google.android.exoplayer.extractor.mp3.Mp3Extractor;
-import com.google.android.exoplayer.extractor.mp4.FragmentedMp4Extractor;
-import com.google.android.exoplayer.extractor.mp4.Mp4Extractor;
-import com.google.android.exoplayer.extractor.ts.AdtsExtractor;
-import com.google.android.exoplayer.extractor.webm.WebmExtractor;
-import com.google.android.exoplayer.util.Util;
+
 
 /**
  * Created by alonzilberman on 7/27/15.
  */
-public class FragmentPlayer extends Fragment {
-    public static final int TYPE_DASH = 0;
-    public static final int TYPE_SS = 1;
-    public static final int TYPE_HLS = 2;
-    public static final int TYPE_MP4 = 3;
-    public static final int TYPE_MP3 = 4;
-    public static final int TYPE_FMP4 = 5;
-    public static final int TYPE_WEBM = 6;
-    public static final int TYPE_MKV = 7;
-    public static final int TYPE_TS = 8;
-    public static final int TYPE_AAC = 9;
-    public static final int TYPE_M4A = 10;
+public class FragmentPlayer extends Fragment implements MediaPlayer.OnPreparedListener {
     public static String TAG = "fragmentPlayer";
 
     private Button btnPlay;
     private RadioItem radioItem;
     private ImageView imgPlayerCover;
     private RelativeLayout rlTopSectionBckg;
-    private int contentType;
-    private MediaPlayer player;
+    private UniversalPlayer universalPlayer;
+
+    private View.OnClickListener btnPlayStopClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (universalPlayer.isPrepaired) {
+                universalPlayer.toggle();
+            } else {
+                universalPlayer.setPreparedListener(FragmentPlayer.this);
+                universalPlayer.prepare(getActivity(), radioItem.getStreamUrl());
+                btnPlay.setText("Fetching...");
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_player, container, false);
         btnPlay = (Button) view.findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(btnPlayStopClickListener);
         rlTopSectionBckg = (RelativeLayout) view.findViewById(R.id.rlTopSectionBckg);
         imgPlayerCover = (ImageView) view.findViewById(R.id.imgPlayerCover);
         if (radioItem != null) {
             initRadioUI(view);
         }
+        universalPlayer = UniversalPlayer.getInstance();
         return view;
     }
 
@@ -75,46 +73,23 @@ public class FragmentPlayer extends Fragment {
                 rlTopSectionBckg.setBackgroundColor(dominantColor);
             }
         });
-        contentType = TYPE_MP3;
-        player = new MediaPlayer(getRendererBuilder());
-        player.prepare();
-        player.setPlayWhenReady(true);
+
     }
 
     public void setRadioItem(RadioItem radioItem) {
         this.radioItem = radioItem;
     }
 
-    private MediaPlayer.RendererBuilder getRendererBuilder() {
-        Uri contentUri = Uri.parse(radioItem.getStreamUrl());
-        String userAgent = Util.getUserAgent(getActivity(), "ExoPlayerDemo");
-        switch (contentType) {
-            // case TYPE_SS:
-            //     return new SmoothStreamingRendererBuilder(getActivity(), userAgent, contentUri.toString(),
-            //             new SmoothStreamingTestMediaDrmCallback());
-            //case TYPE_DASH:
-            //    return new DashRendererBuilder(getActivity(), userAgent, contentUri.toString(),
-            //            new WidevineTestMediaDrmCallback(contentId), audioCapabilities);
-            //case TYPE_HLS:
-            //    return new HlsRendererBuilder(getActivity(), userAgent, contentUri.toString(), audioCapabilities);
-            // case TYPE_M4A: // There are no file format differences between M4A and MP4.
-            case TYPE_MP4:
-                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri, new Mp4Extractor());
-            case TYPE_MP3:
-                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri, new Mp3Extractor());
-            // case TYPE_TS:
-            //return new ExtractorRendererBuilder(getActivity(), userAgent, contentUri,
-            //         new TsExtractor(0, audioCapabilities));
-            case TYPE_AAC:
-                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri, new AdtsExtractor());
-            case TYPE_FMP4:
-                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri,
-                        new FragmentedMp4Extractor());
-            case TYPE_WEBM:
-            case TYPE_MKV:
-                return (MediaPlayer.RendererBuilder) new ExtractorRendererBuilder(getActivity(), userAgent, contentUri, new WebmExtractor());
-            default:
-                throw new IllegalStateException("Unsupported type: " + contentType);
-        }
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        universalPlayer.start();
+        btnPlay.setText("Stop");
+    }
+
+    @Override
+    public void onStop() {
+        universalPlayer.release();
+        universalPlayer = null;
+        super.onStop();
     }
 }
