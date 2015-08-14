fcfe02a1b5fd264ee87d6448e3f1c65ceaaa0f33
alonzilberman <alon.milo@gmail.com>
2015-08-14 20:56:29 +0300

    Stream link choice improved

--- app/src/main/java/com/chickenkiller/upods2/utils/GlobalUtils.java
+++ app/src/main/java/com/chickenkiller/upods2/utils/GlobalUtils.java
@@ -1,5 +1,7 @@
 package com.chickenkiller.upods2.utils;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -8,7 +10,7 @@ import org.json.JSONException;
  */
 public class GlobalUtils {
 
-    private static final String[] bestStreamPatterns = {"\\.mp3", "[^.]{4}$"};
+    private static final String[] bestStreamPatterns = {".+\\.mp3", ".+[^.]{4}$"};
 
 
     public static String getBestStreamUrl(JSONArray allUrls) {
@@ -18,9 +20,11 @@ public class GlobalUtils {
             for (String pattern : bestStreamPatterns) {
                 for (int i = 0; i < allUrls.length(); i++) {
                     if (allUrls.getString(i).matches(pattern)) {
+                        Log.i("MATCHES", allUrls.getString(i) + "pattern " + pattern);
                         url = allUrls.getString(i);
                         return url;
                     }
+                    Log.i("NOT", allUrls.getString(i) + "pattern" + pattern);
                 }
             }
         } catch (JSONException e) {
