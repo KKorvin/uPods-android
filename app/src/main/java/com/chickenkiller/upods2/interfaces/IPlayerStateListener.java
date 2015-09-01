abef554d684c790f4748c8f87d840a6d79f7428d
Alex Berber <berber.it@gmail.com>
2015-09-02 00:44:35 +0300

    - Episodes design added

--- app/src/main/java/com/chickenkiller/upods2/interfaces/IPlayerStateListener.java
+++ app/src/main/java/com/chickenkiller/upods2/interfaces/IPlayerStateListener.java
@@ -4,8 +4,9 @@ import com.chickenkiller.upods2.controllers.UniversalPlayer;
 
 /**
  * Created by alonzilberman on 7/31/15.
- */
+*/
 public interface IPlayerStateListener {
 
     void onStateChanged(UniversalPlayer.State state);
 }
+
