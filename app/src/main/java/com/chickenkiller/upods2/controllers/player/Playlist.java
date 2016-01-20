17563ba7d4e9a61c8264051b9ac441a01eca12dc
alonzilberman <alon.milo@gmail.com>
2016-01-20 17:51:24 +0200

    Player button ripple effect (to test) + clean up

--- app/src/main/java/com/chickenkiller/upods2/controllers/player/Playlist.java
+++ app/src/main/java/com/chickenkiller/upods2/controllers/player/Playlist.java
@@ -254,10 +254,6 @@ public class Playlist implements AdapterView.OnItemClickListener {
 
     private void changeTrack(int position) {
         UniversalPlayer universalPlayer = UniversalPlayer.getInstance();
-        /*if (isOpen) {
-            runCloseAnimation();
-            isOpen = false;
-        }*/
         if (playlistAdapter instanceof PlaylistMediaItemsAdapter) {
             IPlayableMediaItem clickedIPlayableMediaItem = ((PlaylistMediaItemsAdapter) playlistAdapter).getItem(position);
             if (universalPlayer.isCurrentMediaItem(clickedIPlayableMediaItem)) {
