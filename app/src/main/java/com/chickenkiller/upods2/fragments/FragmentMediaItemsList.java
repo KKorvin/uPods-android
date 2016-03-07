33acfe1912fa5f66ccff38573ee41932888b2f70
alonzilberman <alon.milo@gmail.com>
2016-03-07 17:46:29 +0200

    Logic + text + basic UI for empty screens. Dialogs library update.

--- app/src/main/java/com/chickenkiller/upods2/fragments/FragmentMediaItemsList.java
+++ app/src/main/java/com/chickenkiller/upods2/fragments/FragmentMediaItemsList.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.chickenkiller.upods2.R;
 import com.chickenkiller.upods2.controllers.adaperts.MediaItemsAdapter;
@@ -32,6 +34,7 @@ public class FragmentMediaItemsList extends Fragment implements IUpdateableFragm
 
     public static final String TAG;
     private RecyclerView rvMediaItems;
+    private LinearLayout lnEmptyScreen;
     private MediaItemsAdapter mediaItemsAdapter;
     private MediaItemType mediaItemType;
 
@@ -44,29 +47,36 @@ public class FragmentMediaItemsList extends Fragment implements IUpdateableFragm
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_horizontal_media_items, container, false);
         rvMediaItems = (RecyclerView) view.findViewById(R.id.rvMediaItems);
+        lnEmptyScreen = (LinearLayout) view.findViewById(R.id.lnEmptyScreen);
+
+        TextView tvEmptyScreenText = (TextView) view.findViewById(R.id.tvEmptyScreenText);
 
         ArrayList<MediaItem> allItems = new ArrayList<>();
         MediaItemTitle mediaItemTitle = null;
 
         if (mediaItemType == MediaItemType.PODCAST_DOWNLOADED || mediaItemType == null) {
+            tvEmptyScreenText.setText(R.string.empty_dowloaded_podcasts);
             mediaItemTitle = new MediaItemTitle(getString(R.string.recently_downloaded));
             mediaItemTitle.showButton = false;
             allItems.add(mediaItemTitle);
             ArrayList<Podcast> downloadedPodcasts = ProfileManager.getInstance().getDownloadedPodcasts();
             allItems.addAll(downloadedPodcasts);
         } else if (mediaItemType == MediaItemType.PODCAST_FAVORITE) {
+            tvEmptyScreenText.setText(R.string.empty_favorite_podcasts);
             mediaItemTitle = new MediaItemTitle(getString(R.string.recently_subscribed));
             mediaItemTitle.showButton = false;
             allItems.add(mediaItemTitle);
             ArrayList<Podcast> favoritePodcasts = ProfileManager.getInstance().getSubscribedPodcasts();
             allItems.addAll(favoritePodcasts);
         } else if (mediaItemType == MediaItemType.RADIO_SUBSCRIBED) {
+            tvEmptyScreenText.setText(R.string.empty_radio_subscribed);
             mediaItemTitle = new MediaItemTitle(getString(R.string.recently_subscribed));
             mediaItemTitle.showButton = false;
             allItems.add(mediaItemTitle);
             ArrayList<RadioItem> subscribedRadioItems = ProfileManager.getInstance().getSubscribedRadioItems();
             allItems.addAll(subscribedRadioItems);
         } else if (mediaItemType == MediaItemType.RADIO_RECENT) {
+            tvEmptyScreenText.setText(R.string.empty_radio_recent);
             mediaItemTitle = new MediaItemTitle(getString(R.string.recently_played));
             mediaItemTitle.showButton = false;
             allItems.add(mediaItemTitle);
@@ -81,7 +91,13 @@ public class FragmentMediaItemsList extends Fragment implements IUpdateableFragm
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(OrientationHelper.VERTICAL);
         rvMediaItems.setLayoutManager(layoutManager);
-        rvMediaItems.setAdapter(new SlideInBottomAnimatorAdapter(mediaItemsAdapter,rvMediaItems));
+        rvMediaItems.setAdapter(new SlideInBottomAnimatorAdapter(mediaItemsAdapter, rvMediaItems));
+
+        if (mediaItemsAdapter.getItemCount() < 2) {
+            rvMediaItems.setVisibility(View.GONE);
+            lnEmptyScreen.setVisibility(View.VISIBLE);
+        }
+
         return view;
     }
 
@@ -109,5 +125,13 @@ public class FragmentMediaItemsList extends Fragment implements IUpdateableFragm
         mediaItemsAdapter.clearItems();
         mediaItemsAdapter.addItems(allItems);
         mediaItemsAdapter.notifyDataSetChanged();
+
+        if (mediaItemsAdapter.getItemCount() < 2) {
+            rvMediaItems.setVisibility(View.GONE);
+            lnEmptyScreen.setVisibility(View.VISIBLE);
+        } else {
+            rvMediaItems.setVisibility(View.VISIBLE);
+            lnEmptyScreen.setVisibility(View.GONE);
+        }
     }
 }
